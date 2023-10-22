fbe99ab617190f7c8cca5c35c93a22f72e22ce35
yolthurix <146765325+yolthurix@users.noreply.github.com>
2023-10-22 05:00:09 +0300

    fix code by inlining yield

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include "stdlib.h"
 
 struct Task {
+    int id;
     void *stack_pointer;
     void *base_pointer;
     void *instruction_pointer;
@@ -14,7 +15,7 @@ int current_index_to_add = 0;
 
 void queue_task(void (*func)(), void *stack_pointer, void *base_pointer) {
     if (stack_pointer == NULL) {
-        task_queue[current_index_to_add].stack_pointer = malloc(100*sizeof(char)) + 80;
+        task_queue[current_index_to_add].stack_pointer = malloc(1000*sizeof(char)) + 500;
         task_queue[current_index_to_add].base_pointer = task_queue[current_index_to_add].stack_pointer;
     } else {
         task_queue[current_index_to_add].stack_pointer = stack_pointer;
@@ -24,6 +25,8 @@ void queue_task(void (*func)(), void *stack_pointer, void *base_pointer) {
     task_queue[current_index_to_add].instruction_pointer = func;
 
     current_index_to_add++;
+
+    return;
 }
 
 void handle_function_return() {
@@ -44,26 +47,24 @@ void handle_function_return() {
         : "r" (task_queue[current_index_to_execute++].instruction_pointer));
 }
 
-//havainto: jos funktio sisältää funktio callin, funktio laittaa stack pointterin alaspäin funktio callia varten
-
-void yield() {
-    void *return_address;
+__always_inline
+inline void yield() {
+    void *return_address = &&label;
     void *original_base_pointer;
     void *original_stack_pointer;
 
-    asm ("mov %0, [rbp];"
+    asm ("mov %0, rbp;"
             : "=r" (original_base_pointer));
 
-    asm ("mov %0, [rbp+8];"
-            : "=r" (return_address));
+    //asm ("mov %0, [rbp+8];"
+    //        : "=r" (return_address));
 
-    asm ("mov %0, [rbp+16];"
+    asm ("mov %0, rsp;"
             : "=r" (original_stack_pointer));
 
     queue_task(return_address, original_stack_pointer, original_base_pointer);
 
     // setupping to go to next task
-    asm ("leave;");
 
     asm ("mov rsp, %0;"
         :
@@ -80,26 +81,19 @@ void yield() {
     asm ("jmp %0;"
         :
         : "r" (task_queue[current_index_to_execute++].instruction_pointer));
+    
+    label: ;
 }
 
 void test_func() {
     int x = 1 + 2;
-    printf("test\n");
+    printf("%d\n", x);
     return;
 }
 
 int main() {
     queue_task(test_func, NULL, NULL);
     yield();
-
-    char *stack = malloc(100*sizeof(char));
-    asm ("mov rsp, %0;"
-            :
-            : "r" (stack+100)
-            :);
-    printf("%p\n", stack);
-    printf("%p\n", &stack);
-
-    test_func();
+    
     return 0;
 }
