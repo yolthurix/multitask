e624d072f709414d3ff86e0adf4e28b4f3ba5cd6
yolthurix <146765325+yolthurix@users.noreply.github.com>
2023-10-24 23:16:46 +0300

    Create io_multiplexer.c

---
+++ examples/io_multiplexer.c
@@ -0,0 +1,110 @@
+#include "poll.h"
+#include "sys/socket.h"
+#include "netinet/in.h"
+#include "../multitask.h"
+
+struct pollfd polling_fds[10] = { 0 };
+
+struct Task *in_event_callbacks[10] = { 0 };
+struct Task *out_event_callbacks[10] = { 0 };
+
+int number_of_fds = 0;
+
+struct pollfd *fd_to_pollfd(int fd) {
+    for (int i = 0; i < number_of_fds; i++) {
+        if (polling_fds[i].fd == fd) {
+            return &polling_fds[i];
+        }
+    }
+}
+
+void poll_events() {
+    while (1) {
+        int ready = poll(polling_fds, number_of_fds, 0);
+
+        if (ready != 0) {
+            for (int i = 0; i < number_of_fds; i++) {
+                if (polling_fds[i].revents & POLLIN) {
+                    queue_task(*in_event_callbacks[i]);
+                }
+
+                if (polling_fds[i].revents & POLLOUT) {
+                    queue_task(*out_event_callbacks[i]);
+                }
+            }
+        }
+
+        yield(true);
+    }
+}
+
+int setup_server_socket() {
+    struct sockaddr_in address;
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(25565);
+
+    int addrlen = sizeof(address);
+
+    int server_fd = socket(AF_INET, SOCK_STREAM | SOCK_NONBLOCK, 0);
+
+    bind(server_fd, (struct sockaddr*)&address, addrlen);
+
+    listen(server_fd, 10);
+
+    return server_fd;
+}
+
+void set_event_callback(int fd, short events, struct Task *callback_task) {
+    struct pollfd *pfd = fd_to_pollfd(fd);
+
+    pfd->events = events;
+
+    if (events == POLLIN) 
+        in_event_callbacks[pfd - polling_fds] = callback_task;
+    else
+        out_event_callbacks[pfd - polling_fds] = callback_task;
+}
+
+int await_accept(int fd) {
+    struct Task callback_task = create_task(&&callback, false, 0);
+
+    set_event_callback(fd, POLLIN, &callback_task);
+
+    printf("yielding\n");
+
+    yield(false);
+    callback: ;
+
+    printf("called back\n");
+
+    return accept(fd, NULL, NULL);
+}
+
+void test_func(char *string) {
+    while (1) {
+        printf("%s\n", string);
+        yield(true);
+    }
+}
+
+//void await_read() {}
+//void await_write() {}
+
+int main() {
+    char *string = "array of characters";
+
+    queue_task(create_task(poll_events, true, 0));
+    queue_task(create_task(test_func, true, 1, string));
+
+    int server_fd = setup_server_socket();
+
+    polling_fds[0].fd = server_fd;
+    number_of_fds++;
+
+    while (1) {
+        int fd = await_accept(server_fd);
+        printf("client connected\n");
+    }
+    return;
+}
